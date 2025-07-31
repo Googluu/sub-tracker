baea61f88d57a7f6f5fc8f517b6231b06389ed90
Googluu <sneyder.h.rodriguez@gmail.com>
2025-07-31 14:12:03 -0500

    middlewares

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import cookieParser from "cookie-parser";
+
 import express from "express";
 
 import { PORT } from "./config/env.js";
@@ -6,12 +8,22 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subcriptionRouter from "./routes/subcription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json()); // permite manejar JSON en las peticiones
+app.use(express.urlencoded({ extended: true })); // permite manejar datos de formularios html
+app.use(cookieParser()); // permite manejar cookies en las peticiones
+// app.use(cors()); // permite manejar CORS, si es necesario
+// app.use(compression()); // permite comprimir las respuestas HTTP
+// app.use(express.static("public")); // sirve archivos estaticos desde la carpeta public
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subcriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
     res.send({body: "hello world"});
